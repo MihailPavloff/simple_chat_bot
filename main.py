1b1081223e0b0f4f26e5bc2a0f214390a7b0025b
MihailPavloff <87079189+MihailPavloff@users.noreply.github.com>
2022-09-26 17:15:04 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Hello! My name is Vici.")
+print("I was created in 2022")
+print("Please, remind me your name.")
+name = input()
+print("What a great name you have,", name)
+print("Let me guess your age.")
+print("Enter remainders of dividing your age by 3, 5 and 7.")
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+print("Now I will prove to you that I can count to any number you want.")
+N = int(input())
+i = 0
+while i <= N:
+    print(i, "!")
+    i += 1
+print("Completed, have a nice day!")
